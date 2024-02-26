2767ed28bbf3530783f8a5472d828cab11754bf7
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-02-25 21:33:53 -0300

    Commented out unused code in page.tsx and CardPageProject.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,41 +1,41 @@
-"use client";
+// "use client";
 
-import { useState, useEffect } from "react";
-import CardPageProject from "@/components/CardPageProject";
+// import { useState, useEffect } from "react";
+// import CardPageProject from "@/components/CardPageProject";
 
-function Projects() {
-  const [data, setData] = useState([]);
+// function Projects() {
+//   const [data, setData] = useState([]);
 
-  const urlApi = process.env.API_URL || '';
+//   // const urlApi = process.env.API_URL || '';
 
-  useEffect(() => {
-    fetch(urlApi)
-      .then((response) => response.json())
-      .then((data) => {
-        const projects = data.items || [];
-        const images = data.includes.Asset || [];
-        const projectsWithImages = projects.map((project: any) => {
-          const imageId = project.fields.image.sys.id;
-          const image = images.find((img: any) => img.sys.id === imageId);
+//   useEffect(() => {
+//     fetch(urlApi)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const projects = data.items || [];
+//         const images = data.includes.Asset || [];
+//         const projectsWithImages = projects.map((project: any) => {
+//           const imageId = project.fields.image.sys.id;
+//           const image = images.find((img: any) => img.sys.id === imageId);
 
-          return {
-            ...project,
-            imageUrl: image?.fields?.file?.url || null,
-          };
-        });
-        setData(projectsWithImages);
-      });
-  }, [urlApi]);
+//           return {
+//             ...project,
+//             imageUrl: image?.fields?.file?.url || null,
+//           };
+//         });
+//         setData(projectsWithImages);
+//       });
+//   }, [urlApi]);
 
-  return (
-    <div className="w-auto h-screen p-4">
-      <section>
-        <div>
-          <CardPageProject data={data} />
-        </div>
-      </section>
-    </div>
-  );
-}
+//   return (
+//     <div className="w-auto h-screen p-4">
+//       <section>
+//         <div>
+//           <CardPageProject data={data} />
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
 
-export default Projects;
+// export default Projects;
