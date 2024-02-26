248b984efc889085c414ec319f90e7d7b1edd27c
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-02-26 19:10:19 -0300

    Remove loading and projects page components

--- src/components/ButtonCTA.tsx
+++ src/components/ButtonCTA.tsx
@@ -6,7 +6,7 @@ export const ButtonCTA = () => {
     <Link href="https://bit.ly/wsp-dev" target="_blank">
       <button
         type="button"
-        className="text-black bg-white hover:bg-black hover:text-white border-2 border-black font-medium rounded-lg text-sm px-6 py-4 text-center shadow-2xl"
+        className="text-black bg-white hover:bg-black hover:text-white border-2 border-black font-medium rounded-lg text-sm px-6 py-4 text-center shadow-xl"
       >
         <div className="flex justify-center items-center gap-3 shadow-2xl font-bold">
           ¡ Quiero Impulsar mi Negocio ! <RiWhatsappFill className="text-green-500" size={20}/>
