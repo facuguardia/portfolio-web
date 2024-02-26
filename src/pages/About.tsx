5f3938c8b87137c45dfd79178209944192e7c411
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-02-26 19:49:06 -0300

    Update About page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -22,19 +22,30 @@ function About() {
           Acerca <strong>de</strong>
         </h1>
         <p className="mt-5 text-gray-600">
-          Soy un apasionado desarrollador web y móvil. Mi misión es dar vida a tus proyectos más ambiciosos y transformarlos en realidades digitales que brillen con luz propia. Mi conocimiento en desarrollo de software me permite crear aplicaciones con diseños atractivos y funcionales. La palabra "imposible" no existe en mi diccionario. Si tenés una idea, juntos la hacemos realidad.
+          Soy un apasionado desarrollador web y móvil. Mi misión es dar vida a
+          tus proyectos más ambiciosos y transformarlos en realidades digitales
+          que brillen con luz propia. Mi conocimiento en desarrollo de software
+          me permite crear aplicaciones con diseños atractivos y funcionales. La
+          palabra<strong> imposible </strong>no existe en mi diccionario. Si
+          tenés una idea, juntos la hacemos realidad.
           <br />
           <br />
           <strong>Filosofía de Trabajo</strong>
           <br />
           <br />
-          De frente y sin vueltas. Te presto toda mi atención, te asesoro y guío en el proceso de desarrollo de tu proyecto. Siempre con la mejor onda y predisposición. Para que ambos ganemos en este proceso.
+          De frente y sin vueltas. Te presto toda mi atención, te asesoro y guío
+          en el proceso de desarrollo de tu proyecto. Siempre con la mejor onda
+          y predisposición. Para que ambos ganemos en este proceso.
           <br />
           <br />
           <strong>Convertir Tu Visión en Realidad</strong>
           <br />
           <br />
-          Si estás buscando convertir tu idea en un negocio digital rentable... deja de gastar tu dinero en los "vende humo" de siempre, yo llevo tu visión al siguiente nivel, construyendo experiencias digitales que superen tus expectativas. ¡Hablemos sobre tu proyecto y demos vida a tus ideas!
+          Si estás buscando convertir tu idea en un negocio digital rentable...
+          deja de gastar tu dinero en los<strong> vende humo </strong>de siempre, yo llevo tu
+          visión al siguiente nivel, construyendo experiencias digitales que
+          superen tus expectativas. ¡Hablemos sobre tu proyecto y demos vida a
+          tus ideas!
         </p>
       </div>
     </div>
