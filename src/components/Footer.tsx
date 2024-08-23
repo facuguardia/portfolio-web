44fe287ac256be332f7b138b334c5d1b433a5133
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-08-23 14:06:29 +0200

    restyling pircing - about - footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,19 +19,19 @@ export const Footer = () => {
         <div className="font-medium flex flex-col lg:flex-row justify-center items-center gap-2 ">
           <div className="text-xs lg:text-lg text-center lg:text-right items-center">
             <p className="text-sm text-center text-gray-400 md:text-left">
-              © 2023. Todos los derechos reservados.
+              © FacuTech 2023. Todos los derechos reservados.
             </p>
           </div>
 
-          <div className="flex justify-center gap-1">
+          <div className="flex md:flex-col items-center ml-5 gap-2">
             <Link
               href="https://www.linkedin.com/in/facu-guardia/"
               target="_blank"
             >
-              <SocialButtons icon={BiLogoLinkedin} size={20} />
+              <SocialButtons icon={BiLogoLinkedin} size={30} />
             </Link>
             <Link href="https://github.com/facuguardia" target="_blank">
-              <SocialButtons icon={BiLogoGithub} size={20} />
+              <SocialButtons icon={BiLogoGithub} size={30} />
             </Link>
           </div>
         </div>
