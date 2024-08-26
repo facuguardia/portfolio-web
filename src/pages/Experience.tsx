2516c0813f709696736366488b64c42aac199d63
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-08-26 11:30:27 +0200

    add branch new restyling

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -9,31 +9,24 @@ function Experience() {
   return (
     <div
       id="experience"
-      className=" bg-black h-[100%] flex flex-col justify-center items-center pt-24 pb-24 px-3"
+      className=" bg-black w-screen h-full flex flex-col lg:flex-row justify-evenly items-center pt-24 pb-24 px-3 gap-8 lg:gap-4"
     >
-      <div className="container mx-auto w-[95%] mt-10 md:flex md:justify-center">
+      <div>
         <CardExperience
-          title="Soluciones Personalizadas de Aplicaciones Móviles"
-          description="Comprendo la importancia de conectar con tus clientes en cualquier momento y lugar. Por eso, me especializo en el desarrollo de aplicaciones móviles que garantizan una experiencia de usuario fluida y accesible desde cualquier dispositivo."
+          title="Apps Móviles"
           icon={FcAndroidOs}
-          size={40}
+          size={130}
         />
       </div>
-      <div className="container mx-auto w-[95%] mt-10 md:flex md:justify-center">
+      <div>
         <CardExperience
-          title="Desarrollo Integral para Tu Negocio Digital"
-          description="Tu proyecto necesita una solución que combine un diseño atractivo con una funcionalidad sólida. Con mi experiencia en desarrollo, integro habilidades para crear desde interfaces de usuario intuitivas hasta sistemas complejos, utilizando tecnologías de última generación."
+          title="Desarrollo FullStack"
           icon={FcCommandLine}
-          size={40}
+          size={130}
         />
       </div>
-      <div className="container mx-auto w-[95%] mt-10 md:flex md:justify-center ">
-        <CardExperience
-          title="Experiencias Web que Enganchan y Convierten"
-          description="Cada punto de contacto con tu cliente es una oportunidad para impresionar y convertir. Como desarrollador, pongo especial atención en crear sitios web que no solo se vean bien, sino que sean intuitivos y fáciles de navegar. Desarrollo soluciones que capturan la esencia de tu marca y la comunican de manera efectiva, garantizando que cada usuario se sienta involucrado y listo para tomar acción."
-          icon={FcGoogle}
-          size={40}
-        />
+      <div>
+        <CardExperience title="Desarrollo Web" icon={FcGoogle} size={130} />
       </div>
     </div>
   );
